59eef2f6529857ce7713f337db7d2666ef7a51a9
Anwar Rahim <anwargithub@gmail.com>
2024-12-10 15:20:12 +0500

    changes

--- script.js
+++ script.js
@@ -28,15 +28,15 @@ function getFeedHtml(){
                         <p class="tweet-text">${tweetData.tweetText}</p>
                         <div class="tweet-details">
                     <span class="tweet-detail">
-                    ${<i class="fa-regular fa-comment-dots"></i>}
+                     <i class="fa-regular fa-comment-dots"></i>  
                         ${tweetData.replies.length}
-                 
                     </span>
                             <span class="tweet-detail">
-                            ${<i class="fa-regular fa-comment-dots"></i>}
+                            <i class="fa-solid fa-heart"></i>
                         ${tweetData.likes}
                     </span>
                             <span class="tweet-detail">
+                            <i class="fa-solid fa-retweet"></i>
                         ${tweetData.retweets}
                     </span>
                         </div>
